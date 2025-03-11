a4e04315d24d863fc1bc21d2ac07ac9df97cdac2
LaurentChen88 <167003548+LaurentChen88@users.noreply.github.com>
2025-03-11 12:04:12 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="City Fighting",   # Le titre 
+    page_icon="🚀",              # Une icône
+    layout="centered"             # "wide" pour une mise en page large
+)
+
+streamlit.write("""
+# City Fighting
+""")
