eab6ecd8867d6ad9eecff70a716f756119222292
ThomaasSB <thomas.belaid.pro@gmail.com>
2025-04-24 12:11:20 +0200

    Changement Météo et filtre plus ajout

--- Programme_base_donnée/test.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd 
-df_final=pd.read_csv("data/data_final3.csv",sep=",")
-df_final['code_commune_INSEE'] = df_final['code_commune_INSEE'].apply(lambda x: str(x).zfill(5))
-print(df_final)
