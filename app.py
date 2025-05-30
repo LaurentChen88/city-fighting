17ad008874fd615254c379ba766a3b617aedf900
Laurent CHEN <167003548+LaurentChen88@users.noreply.github.com>
2025-05-30 18:16:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -713,11 +713,11 @@ try:
         query = st.text_input("Poste recherché", "Data Analyst")
 
         # Renseigne ici tes propres identifiants
-        client_id = st.secrets.get("client_id", "PAR_outildecisionnel_54f7361a496d42854e4356df6568a5346eba4b9468c9b0067add09ba9f318a4c")
         CLIENT_ID = os.getenv('CLIENT_ID') 
-        client_secret = st.secrets.get("client_secret", "0c1a9a6ed2afcc5f1a730e0a1a747dfc8cd81d039ec95005f851bac1e206d216")
+        client_id = st.secrets.get("client_id", CLIENT_ID)
         CLIENT_SECRET = os.getenv('CLIENT_SECRET') 
-
+        client_secret = st.secrets.get("client_secret", CLIENT_SECRET)
+        
         type_contrat = st.selectbox(
             "Type de contrat",
             options=["", "CDI", "CDD", "MIS", "SAI", "LIB", "AUTRE"],
@@ -733,7 +733,7 @@ try:
 
 
         if st.button("Rechercher"):
-            access_token = get_access_token(CLIENT_ID, CLIENT_SECRET)
+            access_token = get_access_token(client_id, client_secret)
 
             if access_token:
                 cols = st.columns(st.session_state.num_cities)
